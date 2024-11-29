2df4ddd43c84251f5f68d93671e75003c47d4807
DavideVulpes <s8901853u@majorana-giorgi.edu.it>
2024-11-29 12:30:13 +0100

    Update Client 3

--- clientJava/clientJava/src/clientJava/Client.java
+++ clientJava/clientJava/src/clientJava/Client.java
@@ -9,9 +9,23 @@ public class Client {
         int port = 31114; // Porta del server
         
         try {
-            // Crea un socket per connettersi al server
-            Socket socket = new Socket(serverAddress, port);
+        	 // Crea un oggetto BufferedReader per leggere input da riga di comando
+        	 BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        	 
             
+        	 String scelta = "no";
+        	
+        	 while (scelta.equalsIgnoreCase("no")) {
+        		 System.out.println("Vuoi connetterti?");
+             	scelta = consoleReader.readLine();
+             }
+        		
+
+            
+        	// Crea un socket per connettersi al server
+    		Socket socket = new Socket(serverAddress, port);
+    		
+    		
             // Flussi di input e output per comunicare con il server
             OutputStream outputStream = socket.getOutputStream();
             InputStream inputStream = socket.getInputStream();
@@ -22,8 +36,8 @@ public class Client {
             // Lettore per leggere la risposta dal server
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             
-            // Crea un oggetto BufferedReader per leggere input da riga di comando
-            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+           
+           
             
             String messageToSend;
             System.out.println("Scrivi un messaggio da inviare al server (digita 'exit' per uscire):");
@@ -32,15 +46,17 @@ public class Client {
                 // Legge un messaggio dalla riga di comando
                 messageToSend = consoleReader.readLine();
                 
+               
+
+                // Invia il messaggio al server
+                writer.println(messageToSend);
+                System.out.println("Messaggio inviato al server: " + messageToSend);
+                
                 // Se l'utente digita 'exit', esce dal ciclo e chiude la connessione
                 if (messageToSend.equalsIgnoreCase("exit")) {
                     System.out.println("Chiusura della connessione.");
                     break;
                 }
-
-                // Invia il messaggio al server
-                writer.println(messageToSend);
-                System.out.println("Messaggio inviato al server: " + messageToSend);
                 
                 // Legge la risposta del server
                 String serverResponse = reader.readLine();
